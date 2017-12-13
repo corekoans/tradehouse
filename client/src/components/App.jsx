4b8fdecfbd2d00b1d232cb08ee37cf685a86ac14
Brian Leung <bkleung89@users.noreply.github.com>
2017-12-13 11:29:45 -0800

    Update App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -13,13 +13,13 @@ class App extends React.Component {
     return (
       <div>
         <iframe
-          width='560'
-          height='315'
-          src='https://www.youtube.com/embed/live_stream?channel=UCSJ4gkVC6NrvII8umztf0Ow&autoplay=1'
-          frameBorder='0'
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/live_stream?channel=UCSJ4gkVC6NrvII8umztf0Ow&autoplay=1"
+          frameBorder="0"
           allowFullScreen
         />
-        <a className='mercHome' href='/MerchantHome'>Merchant Home</a>
+        <a className="mercHome" href="/MerchantHome">Merchant Home</a>
       </div>
     );
   }
