30824c0f027a5ef5058fd7b9229757e65d44ca42
bkleung89 <bkleung89@gmail.com>
2017-12-19 16:52:17 -0800

    Allow cart and store to retrieve info for logged-in customer.

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -23,6 +23,7 @@ class Header extends React.Component {
     auth.handleAuthentication();
     axios.get(`/api/merchants/bySub/${localStorage.idToken}`)
          .then(res => this.setState({ id: res.data.id }))
+         .catch(err => this.setState({ id: 1 }))
   }
 
   registerFunc() {
@@ -58,13 +59,13 @@ class Header extends React.Component {
           <img src="https://otove.files.wordpress.com/2012/12/tumblr_menq9ehdxq1rj9sw5o1_400.gif" />
         </Link>
         <div className="btn-group">
-          <Link to={`/merchant_profile/${this.state.sub}`} >
+          <Link to={`/merchant_profile/${this.state.id}`} >
 							Merchant Profile
           </Link>
-          <Link to={`/manage_store/${this.state.sub}`}>
+          <Link to={`/manage_store/${this.state.id}`}>
 							Manage Store
           </Link>
-          <Link to={`/broadcast/${this.state.sub}`}>
+          <Link to={`/broadcast/${this.state.id}`}>
 							Broadcast
           </Link>
         </div>
